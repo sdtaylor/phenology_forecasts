c97aa470b212bb70b42b21894a2f8e0822a0bfcc
Shawn Taylor <shawntaylor@weecology.org>
2017-11-19 17:53:03 -0500

    most cfs processing
    -daily means instead of 6 hourly
    -spatially downscaled to prism resolution
    -iterating thru latest ones

--- download_latest_forecasts.py
+++ download_latest_forecasts.py
@@ -1,13 +1,11 @@
 import xarray as xr
 from ftplib import FTP
 import datetime
-import xarray as xr
 import numpy as np
-import pandas as pd
 import yaml
 import os
-import zipfile
 import urllib
+import xmap
 
 
 class cfs_ftp_info:
@@ -57,28 +55,41 @@ class cfs_ftp_info:
         latest_forecast_str = self.latest_forecast_time_str()
         latest_forecast_timestamp = self._string_to_date(latest_forecast_str)
         
-        all_forecasts.append({'forecast_time':latest_forecast_str,
-                              'forecast_path':self._build_full_path(latest_forecast_timestamp)})
+        all_forecasts.append({'initial_time':latest_forecast_str,
+                              'download_url':self._build_full_path(latest_forecast_timestamp)})
         
         six_hours = datetime.timedelta(hours=6)
         for i in range(n-1):
             latest_forecast_timestamp-=six_hours
             latest_forecast_str = self._date_to_string(latest_forecast_timestamp)
             
-            all_forecasts.append({'forecast_time':latest_forecast_str,
-                                  'forecast_path':self._build_full_path(latest_forecast_timestamp)})
+            all_forecasts.append({'initial_time':latest_forecast_str,
+                                  'download_url':self._build_full_path(latest_forecast_timestamp)})
         return all_forecasts
 
-def preprocess_cfs_forecast(cfs, cfs_initial_time):
-    pass
-    # times in the cfs forecasts are a delta from the initial time
-    # convert that to the actual date of the timestep, round to the
-    # day and then take the daily average of the 6 hour forecasts
-    timestamps = np.datetime64(cfs_initial_time) + cfs.forecast_time0.values
-    timestamps_day_only = pd.DatetimeIndex(timestamps, freq='6H').floor('D')
-    cfs['forecast_time0'] = timestamps_day_only
+# CFSv2 is has 6 hour timesteps, convert that to a daily mean
+def cfs_to_daily_mean(cfs, cfs_initial_time):
+    # times in the cfs forecasts are a delta from the initial time.
+    # convert that to the actual date of the timestep.
+    date_timestamps = np.datetime64(cfs_initial_time) + cfs.forecast_time.values
+    cfs['forecast_time'] = date_timestamps
     
+    # Aggregate to daily values instead of 6 hourly
+    cfs = cfs.resample(forecast_time='1D').mean()
     
+    return cfs
+    
+# Put the CFSv2 into a finer grained array
+# This does not account for a different CRS, but actually changing the CRS results
+# in minute differences. It also doesn't explicitely account for CFS being
+# worldwide and prism being N. america. But the internals of xmap (a kdtree lookup)
+# seem to deal with this fine.
+def spatial_downscale(ds, target_array):
+    ds_xmap = xmap.XMap(ds['tmean'])
+    ds_xmap.set_coords(x='lon',y='lat',t='forecast_time')
+    downscaled = ds_xmap.remap_like(target_array, xcoord='lon', ycoord='lat')
+    return downscaled.to_dataset(name='tmean')
+
 def open_cfs_forecast(filename):
     return xr.open_dataset(filename, engine='pynio')
 
@@ -88,34 +99,40 @@ def string_to_date(s, hour=False):
     else:
         return datetime.datetime.strptime(s, '%Y%m%d')
 
-import xmap
-
-prism = xr.open_dataset('~/data/phenology_forecasting/2018_observed_weather.nc')
-
 if __name__=='__main__':
     with open('config.yaml', 'r') as f:
         config = yaml.load(f)
     cfs = cfs_ftp_info()
     
-    cfs_path = '/home/shawn/data/phenology_forecasting/tmp2m.01.2017111218.daily.grb2'
-    cfs_initial_time=string_to_date('2017111218', hour=True)
-    cfs_object = open_cfs_forecast(cfs_path)
+    land_mask = xr.open_dataset(config['data_folder']+config['mask_file'])
+    tmean_names = config['variables_to_use']['tmean']
     
-    test_xmap = xmap.XMap(cfs_object['TMP_P0_L103_GGA0'])
-    test_xmap.set_coords('lon_0','lat_0','forecast_time0')
-    # This works but does not account for a different CRS, and does not explicitely
-    # account for CFS being worldwide and prism being N. america, but deals with
-    # that anyway. Probably something to do with the kdtree nearest neighbor lookup
+    for forecast_info in cfs.last_n_forecasts(n=10):
+        #forecast_info = {'download_url':'/home/shawn/data/phenology_forecasting/tmp2m.01.2017111218.daily.grb2',
+        #                 'initial_time':'2017111218'}
+        forecast_filename = config['tmp_folder']+ os.path.basename(forecast_info['download_url'])
+        urllib.request.urlretrieve(forecast_info['download_url'], forecast_filename)
+
+        # Dowload forecast file
+        #forecast_filename = forecast_info['download_url']
     
-    downscaled_cfs = test_xmap.remap_like(prism['tmean'][0], xcoord='lon', ycoord='lat')
+        forecast_obj = open_cfs_forecast(forecast_filename)
+        initial_time=string_to_date(forecast_info['initial_time'], hour=True)
+        
+        # More reasonable variable names
+        forecast_obj.rename({'lat_0':'lat', 'lon_0':'lon', 
+                             'forecast_time0':'forecast_time',
+                             tmean_names['cfs_nc_name']:'tmean'}, inplace=True)
     
-    #cfs_object = preprocess_cfs_forecast(cfs_object, cfs_initial_time)
+        # Kelvin to celcius
+        forecast_obj['tmean'] -= 273.15
+        
+        # 6 hourly timesteps to daily timesteps
+        forecast_obj = cfs_to_daily_mean(cfs=forecast_obj, cfs_initial_time = initial_time)
     
-    #for forecast in cfs.last_n_forecasts(n=2):
-        #download_url = forecast['forecast_path']
-        #dest_path  = config['tmp_folder']+os.path.basename(download_url)
-        #urllib.request.urlretrieve(download_url, dest_path)
-    
-        #cfs_object = open_cfs_forecast(dest_path)
-    #grib_file = '/home/shawn/data/foo/tmp2m.01.2017111118.daily.grb2'
-    #t = xr.open_dataset(grib_file)
+        # ~1.0 deg cfs grid to 4km prism grid.
+        forecast_obj = spatial_downscale(ds = forecast_obj, target_array = land_mask.to_array()[0])
+        
+        processed_filename = config['data_folder']+'cfsv2_'+forecast_info['initial_time']+'.nc'
+        forecast_obj.to_netcdf(processed_filename)
+        # forecast_obj = statistical_downscale(forecast_obj)
