7183959a548baa24fab1791e444d5d5aa4a4b801
Shawn Taylor <shawntaylor@weecology.org>
2017-11-22 16:40:08 -0500

    update tools name

--- create_mask.py
+++ create_mask.py
@@ -1,18 +1,18 @@
 # Create the north american land mask from a prism file
 import yaml
 import numpy as np
-import prism_utils
+from tools import prism_tools
 
 with open('config.yaml', 'r') as f:
     config = yaml.load(f)
     
 mask_filename = config['data_folder'] + config['mask_file']
 
-prism = prism_utils.prism_ftp_info()
+prism = prism_tools.prism_ftp_info()
 
-prism_date = prism_utils.string_to_date('20100101')
+prism_date = prism_tools.string_to_date('20100101')
 
-prism_array = prism_utils.download_and_process_day(prism, prism_date,
+prism_array = prism_tools.download_and_process_day(prism, prism_date,
                                        varname='tmean',status='stable')
 prism_array = prism_array.drop('time')
 land_values = ~np.isnan(prism_array.tmean.values)
--- download_latest_weather_observations.py
+++ download_latest_weather_observations.py
@@ -5,14 +5,14 @@ import numpy as np
 import pandas as pd
 import yaml
 import os
-from tools import prism_utils
+from tools import prism_tools
 
 with open('config.yaml', 'r') as f:
     config = yaml.load(f)
 
 if __name__=='__main__':
     data_dir = config['data_folder']
-    current_season=prism_utils.current_growing_season()
+    current_season=prism_tools.current_growing_season()
     observed_weather_file = data_dir+current_season+'_observed_weather.nc'
     
     if os.path.isfile(observed_weather_file):
@@ -20,7 +20,7 @@ if __name__=='__main__':
     else:
         print('observed weather file not found')
     
-    today = prism_utils.round_to_current_day(datetime.datetime.today())
+    today = prism_tools.round_to_current_day(datetime.datetime.today())
     yesterday = today - datetime.timedelta(days=1)
     
     # The last entry in this seasons observed weather
@@ -28,14 +28,14 @@ if __name__=='__main__':
     
     prism_days_to_add = pd.date_range(latest_observed_day, np.datetime64(yesterday), closed='right')
     
-    prism = prism_utils.prism_ftp_info()
+    prism = prism_tools.prism_ftp_info()
     
     # TODO: download all of them
     for day in prism_days_to_add[0:3]:
         day = day.to_pydatetime()
         day_status = prism.get_date_status(day)
         if day_status is not None:
-            day_xr= prism_utils.download_and_process_day(prism_info=prism, date=day,
+            day_xr= prism_tools.download_and_process_day(prism_info=prism, date=day,
                                                          varname='tmean', status=day_status)
             observed_weather = observed_weather.combine_first(day_xr)
         else:
@@ -47,17 +47,17 @@ if __name__=='__main__':
     for day in observed_weather.time.values:
         current_status = observed_weather.sel(time=day).status.values.tolist()
         ftp_status = prism.get_date_status(pd.Timestamp(day).to_pydatetime())
-        if prism_utils.newer_file_available(current_status, ftp_status):
+        if prism_tools.newer_file_available(current_status, ftp_status):
             day = pd.Timestamp(day).to_pydatetime()
-            updated_day_xr = prism_utils.download_and_process_day(prism_info=prism, date=day,
+            updated_day_xr = prism_tools.download_and_process_day(prism_info=prism, date=day,
                                                                   varname='tmean', status=ftp_status)
-            observed_weather = prism_utils.update_day(observed_weather, updated_day_xr)
+            observed_weather = prism_tools.update_day(observed_weather, updated_day_xr)
             
             print('file_to_update')
 
     
     prism.close()
-    prism_utils.cleanup_tmp_folder()
+    prism_tools.cleanup_tmp_folder()
 
 
 
