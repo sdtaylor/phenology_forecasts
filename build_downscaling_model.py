2388cc9bd5ce91efd5986c69ef5ffeb9e84761ae
Shawn <shawntaylor@weecology.org>
2017-12-04 17:49:42 -0500

    match forecast data length to observation length

--- build_downscaling_model.py
+++ build_downscaling_model.py
@@ -14,7 +14,7 @@ def collect_obs_data(historic_obs, month, lat=1, lon=1):
     data_values = historic_obs.isel(lat=lat,lon=lon, time=obs_months==month).tmean.values
     data_timestamps = historic_obs.isel(lat=lat,lon=lon, time=obs_months==month).time.values
 
-    return data_values, data_timestamps
+    return {'data':data_values, 'timestamps':data_timestamps}
 
 def collect_forecast_data(forecast_list, month, lead_time, lat=1, lon=1):
     data_values = np.array([])
@@ -38,11 +38,25 @@ def collect_forecast_data(forecast_list, month, lead_time, lat=1, lon=1):
             data_values = np.append(data_values, f.isel(forecast_time=valid_days, lon=lon, lat=lat).tmean.values)
             data_timestamps = np.append(data_timestamps, f.isel(forecast_time=valid_days, lon=lon, lat=lat).forecast_time.values)
 
-    return data_values, data_timestamps
+    return {'data':data_values, 'timestamps':data_timestamps}
 
 
+# Because of many forecasts every month, any given day
+# will have multiple forecasted datapoints. The 
+# observation data needs to be the same length. This will
+# repeat each observed day the appropriate number of times
+# to match the forecast data.
+# f_data and o_data are tuples of (data, timestamps), output
+# from collect_xxx_data()
 def reconcile_forecasts_and_obs(f_data, o_data):
-    pass
+    obs_dates_index=[]
+    # TODO for loops are slow so I could mostly likely improve this
+    for forecast_date in f_data['timestamps']:
+        obs_dates_index.append(np.where(forecast_date==o_data['timestamps'])[0][0])
+    o_data['data'] = o_data['data'][obs_dates_index]
+    o_data['timestamps'] = o_data['timestamps'][obs_dates_index]
+    return f_data, o_data
+
 
 with open('config.yaml', 'r') as f:
     config = yaml.load(f)
@@ -54,6 +68,8 @@ historic_observations = xr.open_dataset(config['historic_observations_file'])
 
 july_forecasts = collect_forecast_data(historic_forecasts, month=7, lead_time=1, lat=300, lon=700)
 july_obs = collect_obs_data(historic_observations, month=7, lat=300, lon=700)
+
+july_forecasts, july_obs = reconcile_forecasts_and_obs(july_forecasts, july_obs)
 
 #for month in range(1,13):
 #    for lead_time in range(1:7):
