fdefa87c97833457b0d5afd0ad9503a05b869470
Shawn Taylor <shawntaylor@weecology.org>
2017-12-13 15:01:54 -0500

    this now works at a reasonable speed
    5 nested for loops is probably not ideal. but estimated time to
    completion is now in hours instead of months. Could probably
    be improved once I figure out xarray.apply_ufunc

--- build_downscaling_model.py
+++ build_downscaling_model.py
@@ -9,12 +9,12 @@ import glob
 import warnings
 
 
-def collect_monthly_data(obj, month, ilat, ilon):
-    obs_months = pd.DatetimeIndex(obj.time.values).month
-    data_values = obj.isel(lat=ilat,lon=ilon, time=obs_months==month).tmean.values
-    data_timestamps = obj.isel(lat=ilat,lon=ilon, time=obs_months==month).time.values
+def collect_monthly_data(obj, ilat, ilon):
+    obj_months = pd.DatetimeIndex(obj.time.values).month
+    data_values = obj.isel(lat=ilat,lon=ilon).tmean.values
+    #data_timestamps = obj.isel(lat=ilat,lon=ilon, time=obs_months==month).time.values
     
-    return data_values
+    return data_values, obj_months
     
 #############################################################
 #############################################################
@@ -24,10 +24,10 @@ with open('config.yaml', 'r') as f:
 land_mask = xr.open_dataset(config['data_folder']+config['mask_file'])
 
 reanalysis_filenames = glob.glob(config['historic_reanalysis_folder']+'cfsv2_reanalysis*')
-reanalysis_obj = xr.open_mfdataset(reanalysis_filenames)
+reanalysis_obj = xr.open_mfdataset(reanalysis_filenames, chunks={'lat':10, 'lon':10})
 
 observation_filenames = glob.glob(config['historic_observations_folder']+'yearly/prism_tmean*')
-observations_obj = xr.open_mfdataset(observation_filenames)
+observations_obj = xr.open_mfdataset(observation_filenames, chunks={'lat':10, 'lon':10})
 
 # create a coefficient array to fill up with values
 empty_array = np.ones((12, land_mask.dims['lat'], land_mask.dims['lon']))
@@ -44,34 +44,66 @@ model_coef=xr.Dataset({'slope':(('month','lat','lon'), empty_array),
 reanalysis_keep = [time in observations_obj.time.values for time in reanalysis_obj.time.values]
 observations_keep = [time in reanalysis_obj.time.values for time in observations_obj.time.values]
 
-reanalysis_obj = reanalysis_obj.isel(time=reanalysis_keep).sortby('time')
-observations_obj = observations_obj.isel(time=observations_keep).sortby('time')
+#reanalysis_obj = reanalysis_obj.isel(time=reanalysis_keep).sortby('time')
+#observations_obj = observations_obj.isel(time=observations_keep).sortby('time')
+reanalysis_obj = reanalysis_obj.isel(time=reanalysis_keep)
+observations_obj = observations_obj.isel(time=observations_keep)
 
-assert np.all(np.equal(observations_obj.time.values, reanalysis_obj.time.values)), 'Dates not matching up'
+#assert np.all(np.equal(observations_obj.time.values, reanalysis_obj.time.values)), 'Dates not matching up'
 
 #########################################
+def chunker(seq, size):
+    return (seq[pos:pos + size] for pos in range(0, len(seq), size))
+########################################
 total_pixels = land_mask.land.values.sum()
 progress=0
 pixel_processing_times=[]
 import time
-for lat_i in range(len(land_mask.lat)):
-    for lon_i in range(len(land_mask.lon)):
-        is_land = land_mask.isel(lat=lat_i,lon=lon_i).land.values
-        if is_land:
-            pixel_start_time=time.time()
-            for month_i, month in enumerate(range(1,13)):
-                reanalysis = collect_monthly_data(reanalysis_obj, month=month, ilat=lat_i, ilon=lon_i)
-                observations = collect_monthly_data(observations_obj, month=month, ilat=lat_i, ilon=lon_i)
+lat_lon_chunk=10
 
-                # The ranking step of the downscaling model
-                reanalysis.sort()
-                observations.sort()
+for lat_chunk in list(chunker(land_mask.lat, lat_lon_chunk)):
+    for lon_chunk in chunker(land_mask.lon, lat_lon_chunk):
+        land_mask_subset = land_mask.sel(lat=lat_chunk, lon=lon_chunk)
+        if np.all(~land_mask_subset.land.values):
+            continue
+       
+        print('chunking')
+        reanalysis_subset = reanalysis_obj.sel(lat=lat_chunk, lon=lon_chunk)
+        observations_subset = observations_obj.sel(lat=lat_chunk, lon=lon_chunk)
+        model_coef_subset = model_coef.sel(lat=lat_chunk, lon=lon_chunk)
 
-                slope, intercept, *_ = lm(x=reanalysis, y=observations)
-
-                model_coef['slope'][month_i, lat_i, lon_i]=slope
-                model_coef['intercept'][month_i, lat_i, lon_i]=intercept
-
-            pixel_processing_times.append(round(time.time() - pixel_start_time, 1))
-            print(str(progress)+'/'+str(total_pixels)+' pixels, '+str(pixel_processing_times[-1])+' sec')
-            print('avg: '+str(np.mean(pixel_processing_times)))
+        print('loading re')
+        reanalysis_subset.load()
+        print('loading obd')
+        observations_subset.load()
+        land_mask_subset.load()
+    
+        print(len(land_mask_subset.lat))
+        for lat_i in range(len(land_mask_subset.lat)):
+            for lon_i in range(len(land_mask_subset.lon)):
+                is_land = land_mask_subset.isel(lat=lat_i,lon=lon_i).land.values
+                if is_land:
+                    pixel_start_time=time.time()
+                    print('collecting')
+        
+                    reanalysis_data, reanalysis_months = collect_monthly_data(reanalysis_subset, ilat=lat_i, ilon=lon_i)
+                    observations_data, observations_months = collect_monthly_data(observations_subset, ilat=lat_i, ilon=lon_i)
+        
+                    for month_i, month in enumerate(range(1,13)):
+                        print('month: '+str(month))
+                        reanalysis = reanalysis_data[reanalysis_months==month].copy()
+                        observations = observations_data[observations_months==month].copy()
+        
+                        # The ranking step of the downscaling model
+                        reanalysis.sort()
+                        observations.sort()
+                        
+                        slope, intercept, *_ = lm(x=reanalysis, y=observations)
+        
+                        model_coef_subset['slope'][month_i, lat_i, lon_i]=slope
+                        model_coef_subset['intercept'][month_i, lat_i, lon_i]=intercept
+        
+                    progress+=1
+                    pixel_processing_times.append(round(time.time() - pixel_start_time, 1))
+                    print(str(progress)+'/'+str(total_pixels)+' pixels, '+str(pixel_processing_times[-1])+' sec')
+                    print('avg: '+str(np.mean(pixel_processing_times)))
