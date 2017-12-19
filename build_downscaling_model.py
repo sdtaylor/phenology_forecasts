31c87728803c9e5b9ccd1edf2d3dc9dad10944d9
Shawn <shawntaylor@weecology.org>
2017-12-19 15:26:03 -0500

    working downscale model, for real this time

--- build_downscaling_model.py
+++ build_downscaling_model.py
@@ -32,8 +32,8 @@ observations_obj = xr.open_mfdataset(observation_filenames, chunks={'lat':10, 'l
 # create a coefficient array to fill up with values
 empty_array = np.ones((12, land_mask.dims['lat'], land_mask.dims['lon']))
 empty_array[:] = np.nan
-model_coef=xr.Dataset({'slope':(('month','lat','lon'), empty_array),
-                       'intercept':(('month','lat','lon'), empty_array)},
+model_coef=xr.Dataset({'slope':(('month','lat','lon'), empty_array.copy()),
+                       'intercept':(('month','lat','lon'), empty_array.copy())},
                       {'lat':land_mask.lat.values, 'lon':land_mask.lon.values,
                        'month':np.arange(1,13)})
 
@@ -69,7 +69,7 @@ lat_lon_chunk=10
 # friendly. 
 
 for lat_chunk in list(chunker(land_mask.lat, lat_lon_chunk)):
-    for lon_chunk in chunker(land_mask.lon, lat_lon_chunk):
+    for lon_chunk in list(chunker(land_mask.lon, lat_lon_chunk)):
         land_mask_subset = land_mask.sel(lat=lat_chunk, lon=lon_chunk)
         if np.all(~land_mask_subset.land.values):
             continue
@@ -77,7 +77,6 @@ for lat_chunk in list(chunker(land_mask.lat, lat_lon_chunk)):
         print('chunking')
         reanalysis_subset = reanalysis_obj.sel(lat=lat_chunk, lon=lon_chunk)
         observations_subset = observations_obj.sel(lat=lat_chunk, lon=lon_chunk)
-        model_coef_subset = model_coef.sel(lat=lat_chunk, lon=lon_chunk).copy()
 
         print('loading re')
         reanalysis_subset.load()
@@ -97,7 +96,7 @@ for lat_chunk in list(chunker(land_mask.lat, lat_lon_chunk)):
                     observations_data, observations_months = collect_monthly_data(observations_subset, ilat=lat_i, ilon=lon_i)
         
                     for month_i, month in enumerate(range(1,13)):
-                        print('month: '+str(month))
+                        #print('month: '+str(month))
                         reanalysis = reanalysis_data[reanalysis_months==month].copy()
                         observations = observations_data[observations_months==month].copy()
         
@@ -106,15 +105,24 @@ for lat_chunk in list(chunker(land_mask.lat, lat_lon_chunk)):
                         observations.sort()
                         
                         slope, intercept, *_ = lm(x=reanalysis, y=observations)
+                        #print(slope)
+                        #print(intercept)
         
-                        model_coef_subset['slope'][month_i, lat_i, lon_i]=slope
-                        model_coef_subset['intercept'][month_i, lat_i, lon_i]=intercept
+                        full_array_lat_i = np.where(land_mask.lat.values == land_mask_subset.lat.values[lat_i])[0][0]
+                        full_array_lon_i = np.where(land_mask.lon.values == land_mask_subset.lon.values[lon_i])[0][0]
+                        model_coef['slope'][month_i, full_array_lat_i, full_array_lon_i]=slope
+                        model_coef['intercept'][month_i, full_array_lat_i, full_array_lon_i]=intercept
         
                     progress+=1
                     pixel_processing_times.append(round(time.time() - pixel_start_time, 1))
                     print(str(progress)+'/'+str(total_pixels)+' pixels, '+str(pixel_processing_times[-1])+' sec')
                     print('avg: '+str(np.mean(pixel_processing_times)))
 
-        model_coef = model_coef.update(model_coef_subset)
+        #model_coef = model_coef.update(model_coef_subset).copy()
+        #print(model_coef_subset.slope.shape)
+        #print('subset slope num nan: '+str(np.sum(~np.isnan(model_coef_subset.slope))))
+        #print('subset intercept num nan: '+str(np.sum(~np.isnan(model_coef_subset.intercept))))
+        print('original slope num nan: '+str(np.sum(~np.isnan(model_coef.slope))))
+        print('original intercpet num nan: '+str(np.sum(~np.isnan(model_coef.intercept))))
 
 model_coef.to_netcdf(config['downscaling_model_coefficients_file'], encoding={'slope':{'zlib':False}, 'intercept':{'zlib':False}})
