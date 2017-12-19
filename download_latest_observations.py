4e7ccbadf33947fe01d82f32a26523b3be94e7e7
Shawn Taylor <shawntaylor@weecology.org>
2017-12-19 15:24:53 -0500

    update all this to the latest stuff

--- download_latest_observations.py
+++ download_latest_observations.py
@@ -5,39 +5,50 @@ import numpy as np
 import pandas as pd
 import yaml
 import os
-from tools import prism_tools
+from tools import prism_tools, tools
 
-with open('config.yaml', 'r') as f:
-    config = yaml.load(f)
+config = tools.load_config()
 
 if __name__=='__main__':
-    data_dir = config['data_folder']
     current_season=prism_tools.current_growing_season()
-    observed_weather_file = data_dir+current_season+'_observed_weather.nc'
+    observed_weather_file = config['current_season_observations_file']
+    
+    today = datetime.datetime.today().date()
+    yesterday = today - datetime.timedelta(days=1)
+    
+    prism = prism_tools.prism_ftp_info()
+
     
     if os.path.isfile(observed_weather_file):
         observed_weather = xr.open_dataset(observed_weather_file)
+        # The last entry in this seasons observed weather
     else:
-        print('observed weather file not found')
+        # Initiate this seasons observed weather
+        first_day = str(int(current_season)-1) + config['season_month_begin'] + config['season_day_begin']
+        first_day = tools.string_to_date(first_day, h=False).date()
+        assert first_day < today, 'Beginning of season hasnt happened yet'
+        first_day_url = prism.get_download_url(first_day)
+        first_day_status = prism.get_date_status(first_day)
+        observed_weather = prism_tools.download_and_process_day(download_url=first_day_url,
+                                                                date=first_day,
+                                                                varname='tmean',
+                                                                status=first_day_status)
     
-    today = prism_tools.round_to_current_day(datetime.datetime.today())
-    yesterday = today - datetime.timedelta(days=1)
-    
-    # The last entry in this seasons observed weather
     latest_observed_day = observed_weather.time.values[-1]
-    
     prism_days_to_add = pd.date_range(latest_observed_day, np.datetime64(yesterday), closed='right')
     
-    prism = prism_tools.prism_ftp_info()
     
     # TODO: download all of them
-    for day in prism_days_to_add[0:3]:
+    for day in prism_days_to_add:
         day = day.to_pydatetime()
         day_status = prism.get_date_status(day)
+        day_url = prism.get_download_url(day)
         if day_status is not None:
-            day_xr= prism_tools.download_and_process_day(prism_info=prism, date=day,
-                                                         varname='tmean', status=day_status)
-            observed_weather = observed_weather.combine_first(day_xr)
+            day_xr= prism_tools.download_and_process_day(download_url=day_url,
+                                                         date=day,
+                                                         varname='tmean',
+                                                         status=day_status)
+            observed_weather = observed_weather.merge(day_xr)
         else:
             pass
             # make a blank array for this day with status None
@@ -55,9 +66,10 @@ if __name__=='__main__':
             
             print('file_to_update')
 
+    observed_weather.to_netcfs(config['current_season_observations_file'])
     
     prism.close()
-    prism_tools.cleanup_tmp_folder()
+    tools.cleanup_tmp_folder(config['tmp_folder'])
 
 
 
