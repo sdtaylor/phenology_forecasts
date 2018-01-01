1011136f6d23ffc9942a034ccc92c952d3811b5a
Shawn <shawntaylor@weecology.org>
2018-01-01 15:03:53 -0500

    put phenology model building into MPI format

--- build_phenology_models.py
+++ build_phenology_models.py
@@ -2,94 +2,135 @@ import pyPhenology
 import pandas as pd
 import numpy as np
 from tools import tools
-import os
 import uuid
 import time
 import datetime
+from pySimpleMPI.framework import run_MPI
+
 
 config = tools.load_config()
 percent_test = 0.2
+datetime.datetime.today().date()
 potential_base_models = ['ThermalTime','Alternating', 'Uniforc']
-species_info = pd.read_csv(config['species_list_file'])
-
-today = datetime.datetime.today().date()
-
-temperature_obs = pd.read_csv(config['phenology_observations_temperature_file'])
-
-model_metadata=[]
-
 divider='#'*90
 
-for i in range(len(species_info)):
-    species_name=species_info.species[i]
-    phenophase=species_info.Phenophase_ID[i]
-    
-    print(divider)
-    print('Finding best model for {s} {p}'.format(s=species_name, p=phenophase))
-    
-    species_obs = pd.read_csv(config['phenology_observations_folder']+species_info.observation_file[i])
-    species_obs, temperature_obs = pyPhenology.utils.check_data(species_obs, temperature_obs)
+class model_finder_worker:
+    def __init__(self):
+        pass
 
-    testing_obs = species_obs.sample(frac=percent_test, random_state=1, replace=False)
-    training_obs = species_obs[~species_obs.index.isin(testing_obs.index)]
+    def setup(self):
+        self.temperature_obs = pd.read_csv(config['phenology_observations_temperature_file'])
+        self.today = datetime.datetime.today().date()
+        print('seting things up')
     
-    best_aic=np.inf
-    best_base_model = None
-    best_base_model_name = None
+    def run_job(self, species_info):
         
-    for model_name in potential_base_models:
-        Model = pyPhenology.utils.load_model(model_name)
-        model = Model()
-        model.fit(observations=training_obs, temperature=temperature_obs,
-                  optimizer_params={'popsize':100,
-                                    'maxiter':50000})
-        model_aic = tools.aic(obs = testing_obs.doy.values, 
-                              pred = model.predict(testing_obs, temperature_obs),
-                              n_param = len(model._parameters_to_estimate))
-        if model_aic < best_aic:
-            best_base_model = model
-            best_base_model_name = model_name
-            best_aic = model_aic
+        species_name=species_info['species']
+        phenophase=species_info['Phenophase_ID']
         
-        print('model {m} got an aic of {a}'.format(m=model_name,a=model_aic))
-
-    print('Chose {m} for {s} {p}'.format(m=best_base_model_name,
-                                         s=species_name,
-                                         p=phenophase))
-    #######################################################
-    # Save the best model using a unique identifier
-    time.sleep(1)
-    model_hash = str(uuid.uuid1())
-    model_filename = '{s}_{p}_{h}.csv'.format(s=species_info.species[i].replace(' ','_'),
-                                              p=species_info.Phenophase_ID[i],
-                                              h=model_hash)
-
-    best_base_model.save_params(config['phenology_model_folder']+model_filename)
+        print(divider)
+        print('Finding best model for {s} {p}'.format(s=species_name, p=phenophase))
+        
+        species_obs = pd.read_csv(config['phenology_observations_folder']+species_info['observation_file'])
+        species_obs, temperature_obs = pyPhenology.utils.check_data(species_obs, self.temperature_obs)
     
-    #######################################################
-    # Iterate this species/phenophase forecast version
-    try:
-        current_version = species_info.current_forecast_version[i]
-    except:
-        current_version = 0
+        testing_obs = species_obs.sample(frac=percent_test, random_state=1, replace=False)
+        training_obs = species_obs[~species_obs.index.isin(testing_obs.index)]
+        
+        best_aic=np.inf
+        best_base_model = None
+        best_base_model_name = None
+            
+        for model_name in potential_base_models:
+            Model = pyPhenology.utils.load_model(model_name)
+            model = Model()
+            model.fit(observations=training_obs, temperature=temperature_obs,
+                      optimizer_params={'popsize':100,
+                                        'maxiter':50000})
+            model_aic = tools.aic(obs = testing_obs.doy.values, 
+                                  pred = model.predict(testing_obs, temperature_obs),
+                                  n_param = len(model._parameters_to_estimate))
+            if model_aic < best_aic:
+                best_base_model = model
+                best_base_model_name = model_name
+                best_aic = model_aic
+            
+            print('model {m} got an aic of {a}'.format(m=model_name,a=model_aic))
     
-    current_version+=1
+        print('Chose {m} for {s} {p}'.format(m=best_base_model_name,
+                                             s=species_name,
+                                             p=phenophase))
+        #######################################################
+        # Save the best model using a unique identifier
+        time.sleep(1)
+        model_hash = str(uuid.uuid1())
+        model_filename = '{s}_{p}_{h}.csv'.format(s=species_info['species'].replace(' ','_'),
+                                                  p=species_info['Phenophase_ID'],
+                                                  h=model_hash)
+    
+        best_base_model.save_params(config['phenology_model_folder']+model_filename)
+        
+        #######################################################
+        # Iterate this species/phenophase forecast version
+        try:
+            current_version = species_info['current_forecast_version']
+        except:
+            current_version = 0
+        
+        current_version+=1
+    
+        species_info['current_forecast_version']=current_version
+        
+        #######################################################
+        # make entry for this specifc model in model metadata file
+        model_note = """Selected the best model via aic from ThermalTime,Alternating and Uniforc."""
+    
+        model_metadata ={'species':species_info['species'],
+                         'Phenophase_ID':species_info['Phenophase_ID'],
+                         'base_model':best_base_model_name,
+                         'forecast_version':current_version,
+                         'model_file':model_filename,
+                         'build_date':str(self.today),
+                         'n_observations':len(species_obs),
+                         'percent_test':percent_test,
+                         'note':model_note}
+        
+           
+        return {'info':species_info, 'metadata':model_metadata}
+        
+class model_finder_boss:
+    def __init__(self):
+        pass
+    
+    def setup(self):
+        self.species_info = pd.read_csv(config['species_list_file'])
+        
+        self.job_list=self.species_info.to_dict('records')
+        self.total_jobs=len(self.job_list)
 
-    species_info['current_forecast_version'][i]=current_version
-    #######################################################
-    # add entry for this specifc model in model metadata file
-    model_note = """Selected the best model via aic from ThermalTime,Alternating and Uniforc."""
+        self.updated_species_info=[]
+        self.model_metadata=[]
+        self.today = datetime.datetime.today().date()
     
-    model_metadata.append({'species':species_info.species[i],
-                             'Phenophase_ID':species_info.Phenophase_ID[i],
-                             'base_model':best_base_model_name,
-                             'forecast_version':current_version,
-                             'model_file':model_filename,
-                             'build_date':str(today),
-                             'n_observations':len(species_obs),
-                             'percent_test':percent_test,
-                             'note':model_note})
+    def jobs_available(self):
+        return len(self.job_list)>0
+        
+    def get_next_job(self):
+        return self.job_list.pop()
     
+    def process_job_result(self, result):
+        self.updated_species_info.append(result['info'])
+        self.model_metadata.append(result['metadata'])
 
-tools.update_csv(species_info, config['species_list_file'])
-tools.append_csv(pd.DataFrame(model_metadata), config['phenology_model_metadata_file'])
+    def process_all_results(self, all_results):
+        tools.update_csv(pd.DataFrame(self.finished_jobs), config['species_list_file'])
+        tools.append_csv(pd.DataFrame(self.model_metadata), config['phenology_model_metadata_file'])
+        
+if __name__ == "__main__":
+    run_MPI(model_finder_boss(), model_finder_worker())
+
+
+
+
+
+
