a221e4e05e21fed1bd74b1b5f27e93f69b17908f
Shawn Taylor <shawntaylor@weecology.org>
2018-01-21 14:23:36 -0500

    switch metadata to attributes, not data

--- automated_forecasting/phenology/apply_phenology_models.py
+++ automated_forecasting/phenology/apply_phenology_models.py
@@ -122,9 +122,9 @@ def run():
     Plant phenology models made using National Phenology Network data. 
     """.format(s=current_season, t=today)
     
-    all_species_forecasts['note']=provenance_note
-    all_species_forecasts['issue_date']=str(today)
-    all_species_forecasts['crs']='+init=epsg:4269'
+    all_species_forecasts.attrs['note']=provenance_note
+    all_species_forecasts.attrs['issue_date']=str(today)
+    all_species_forecasts.attrs['crs']='+init=epsg:4269'
     # TODO: add some  more metadata
     # common names?
     #all_species_forecasts['forecast_date']=str(today)
