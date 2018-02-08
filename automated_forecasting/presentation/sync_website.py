1c3e0c11eebffcc7cd715a8cbb0e6313879a48b4
Shawn Taylor <shawntaylor@weecology.org>
2018-02-08 14:38:10 -0500

    option to only upload metadata file

--- automated_forecasting/presentation/sync_website.py
+++ automated_forecasting/presentation/sync_website.py
@@ -6,7 +6,7 @@ from tools import tools
 
 # Sync static  images and image metadata to the website on google cloud storage
 
-def run(update=False):
+def run(update_all_images=False, metadata_only=False):
     config = tools.load_config()
     
     # Setup authentation to google cloud
@@ -21,23 +21,24 @@ def run(update=False):
     # Iteration thru all the images and upload if they are not already there
     issue_date_folders = glob.glob(config['phenology_forecast_figure_folder']+'2*')
     
-    for f in issue_date_folders:
-        issue_date = os.path.basename(f)
-        available_images = glob.glob(f+'/*.png')
-        
-        for image_path in available_images:
-            image_filename = os.path.basename(image_path)
-            storage_path = 'images/{issue_date}/{filename}'.format(issue_date=issue_date,
-                                                                   filename=image_filename)
+    if not metadata_only:
+        for f in issue_date_folders:
+            issue_date = os.path.basename(f)
+            available_images = glob.glob(f+'/*.png')
             
-            image_blob = phenology_bucket.blob(storage_path)
-            if image_blob.exists() and not update:
-                pass
-                #print('{i} exists, moving on'.format(i=image_filename))
-            else:
-                print('uploading {i}'.format(i=image_filename))
-                image_blob.upload_from_filename(image_path)
-                image_blob.make_public()
+            for image_path in available_images:
+                image_filename = os.path.basename(image_path)
+                storage_path = 'images/{issue_date}/{filename}'.format(issue_date=issue_date,
+                                                                       filename=image_filename)
+                
+                image_blob = phenology_bucket.blob(storage_path)
+                if image_blob.exists() and not update_all_images:
+                    pass
+                    #print('{i} exists, moving on'.format(i=image_filename))
+                else:
+                    print('uploading {i}'.format(i=image_filename))
+                    image_blob.upload_from_filename(image_path)
+                    image_blob.make_public()
     
     # Update the image metadata file
     metadata_blob=phenology_bucket.blob('image_metadata.json')
