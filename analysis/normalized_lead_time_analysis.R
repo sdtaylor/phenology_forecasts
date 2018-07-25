183a9ebc39b2538512e4c302c44111899b9edb86
Shawn Taylor <shawntaylor@weecology.org>
2018-07-25 11:39:41 -0400

    figures for #ESA2018

--- analysis/normalized_lead_time_analysis.R
+++ analysis/normalized_lead_time_analysis.R
@@ -7,7 +7,8 @@ forecasted_species = read_csv('/home/shawn/data/phenology_forecasting/species_fo
 
 species_habit = forecasted_species %>%
   select(species,habit) %>%
-  distinct()
+  distinct() %>%
+  mutate(habit = tools::toTitleCase(habit))
 
 validation_observations = read_csv(config$phenology_current_season_observation_file) %>%
   filter(interaction(species, Phenophase_ID) %in% interaction(forecasted_species$species, forecasted_species$Phenophase_ID)) %>%
@@ -61,7 +62,7 @@ forecast_data = ensemble_model_forecast
 # ie. if there were forecasts on doy 20 and 25. Then this copies the doy 20 forecast
 # to doy 21-24.
 forecast_data = forecast_data %>%
-  complete(issue_doy=min(issue_doy):(max(issue_doy)+20), nesting(species, Phenophase_ID, site_id, phenology_model)) %>%
+  complete(issue_doy=min(issue_doy):(max(issue_doy)+30), nesting(species, Phenophase_ID, site_id, phenology_model)) %>%
   arrange(issue_doy) %>%
   group_by(species, Phenophase_ID, site_id, phenology_model) %>%
   mutate(doy_prediction = zoo::na.locf(doy_prediction, na.rm=FALSE),
@@ -101,7 +102,7 @@ forecast_data = forecast_data %>%
   filter(lead_time >= -90, lead_time <=0)
 ################################
 # This species seems artificially high, potentially due 
-# to a lof of observations at relatively few sites
+# to a lot of observations at relatively few sites
 forecast_data = forecast_data %>%
   filter(species != 'amelanchier grandiflora-autumnbrilliance')
 ################################
@@ -113,8 +114,8 @@ aggregate_forecast_error = forecast_data %>%
             forecast_error = sqrt(mean((doy_prediction - doy_observed)**2)),
             skill = 1 - (forecast_error/naive_error),
             n=n()) %>%
-  ungroup() 
-aggregate_forecast_error$error_type = 'Overall Error'
+  ungroup()
+aggregate_forecast_error$error_type = 'Overall Error (n=1618)'
 
 genus_forecast_error = forecast_data %>%
   rename(genus=Genus) %>%
@@ -127,7 +128,8 @@ genus_forecast_error = forecast_data %>%
             forecast_error = sqrt(mean((doy_prediction - doy_observed)**2)),
             skill = 1 - (forecast_error/naive_error),
             n=n()) %>%
-  ungroup() 
+  ungroup() %>%
+  mutate(genus_with_n = paste0(genus,' (n=',n,')'))
 
 habit_forecast_error = forecast_data %>%
   left_join(species_habit, by='species') %>%
@@ -136,7 +138,8 @@ habit_forecast_error = forecast_data %>%
             forecast_error = sqrt(mean((doy_prediction - doy_observed)**2)),
             skill = 1 - (forecast_error/naive_error),
             n=n()) %>%
-  ungroup() 
+  ungroup() %>%
+  mutate(habit_with_n = paste0(habit,' (n=',n,')'))
 
 phenophase_forecast_error = forecast_data %>%
   group_by(Phenophase_ID, phenology_model, lead_time) %>%
@@ -145,7 +148,9 @@ phenophase_forecast_error = forecast_data %>%
             skill = 1 - (forecast_error/naive_error),
             n=n()) %>%
   ungroup() 
+
 phenophase_forecast_error$Phenophase_ID = factor(phenophase_forecast_error$Phenophase_ID, levels=c(371,501), labels=c('Budburst','Open Flowers'))
+phenophase_forecast_error$phenophase_with_n = with(phenophase_forecast_error, paste0(Phenophase_ID,' (n=',n,')'))
 
 species_forecast_error = forecast_data %>%
   group_by(species,Phenophase_ID, phenology_model, lead_time) %>%
@@ -158,11 +163,14 @@ species_forecast_error$Phenophase_ID = factor(species_forecast_error$Phenophase_
 
 ######################################################
 x_pos_line= -100
-indicator_lines=data.frame(y=c(-0.1, 0.1), yend=c(-0.2, 0.2),
+indicator_lines=data.frame(y=c(-0.05, 0.05), yend=c(-0.12, 0.12),
                            x=c(x_pos_line,x_pos_line), xend=c(x_pos_line,x_pos_line))
 
-x_pos_text=-99
-indicator_text=data.frame(y=c(-0.27, 0.27), x=x_pos_text, t=c('Worse than\n naive model','Better than\n naive model'))
+x_pos_text=-95
+indicator_text=data.frame(y=c(-0.17, 0.17), x=x_pos_text, t=c('Worse than\n naive model','Better than\n naive model'))
+
+ylim_high = 0.25
+ylim_low  =-0.25
 
 ####
 ggplot(aggregate_forecast_error, aes(x=lead_time, y=skill, color=error_type)) + 
@@ -171,7 +179,7 @@ ggplot(aggregate_forecast_error, aes(x=lead_time, y=skill, color=error_type)) +
   geom_segment(data=indicator_lines, aes(x=x, xend=xend, y=y, yend=yend), size=0.8, arrow = arrow(length=unit(0.25,'cm')),
                inherit.aes = F) +
   geom_text(data=indicator_text, aes(x=x,y=y, label=t),size=4.5, inherit.aes = F) +
-  ylim(-0.5,0.4) +
+  ylim(ylim_low, ylim_high) +
   scale_x_continuous(labels = function(x){x*-1}) + 
   geom_hline(yintercept = 0, linetype='dashed', size=1) + 
   labs(y='Skill',x='Lead time in days', color='') + 
@@ -185,71 +193,74 @@ ggplot(aggregate_forecast_error, aes(x=lead_time, y=skill, color=error_type)) +
         axis.text = element_text(size=12))
   
 #####
-ggplot(phenophase_forecast_error, aes(x=lead_time, y=skill, color=as.factor(Phenophase_ID))) + 
+ggplot(phenophase_forecast_error, aes(x=lead_time, y=skill, color=as.factor(phenophase_with_n))) + 
   geom_line(size=1.5) +
   scale_color_manual(values=c("#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")) + 
   geom_segment(data=indicator_lines, aes(x=x, xend=xend, y=y, yend=yend), size=0.8, arrow = arrow(length=unit(0.25,'cm')),
                inherit.aes = F) +
   geom_text(data=indicator_text, aes(x=x,y=y, label=t),size=4.5, inherit.aes = F) +
-  ylim(-0.5,0.4) +
+  ylim(ylim_low, ylim_high) +
   scale_x_continuous(labels = function(x){x*-1}) + 
   geom_hline(yintercept = 0, linetype='dashed', size=1) + 
   labs(y='Skill',x='Lead time in days', color='') + 
   theme_bw() +
-  theme(legend.position = c(0.8,0.3),
+  theme(legend.position = c(0.6,0.3),
         legend.text = element_text(size=16),
         legend.key.width = unit(2,'cm'),
         legend.key.height = unit(0.5,'cm'),
         axis.title = element_text(size=18),
-        axis.text = element_text(size=12))
+        axis.text = element_text(size=12)) 
 ####
-ggplot(genus_forecast_error, aes(x=lead_time, y=skill, color=as.factor(genus))) + 
+ggplot(genus_forecast_error, aes(x=lead_time, y=skill, color=as.factor(genus_with_n))) + 
   geom_line(size=1.5) +
-  scale_color_manual(values=c("#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")) + 
+  scale_color_manual(values=c("#E69F00", "#009E73", "#999999", "#0072B2", "#CC79A7")) + 
   geom_segment(data=indicator_lines, aes(x=x, xend=xend, y=y, yend=yend), size=0.8, arrow = arrow(length=unit(0.25,'cm')),
                inherit.aes = F) +
   geom_text(data=indicator_text, aes(x=x,y=y, label=t),size=4.5, inherit.aes = F) +
-  ylim(-0.5,0.4) +
+  ylim(ylim_low, ylim_high) +
   scale_x_continuous(labels = function(x){x*-1}) + 
   geom_hline(yintercept = 0, linetype='dashed', size=1) + 
-  ggtitle('By Genus') + 
+  #ggtitle('By Genus') + 
   labs(y='Skill',x='Lead time in days', color='') + 
   theme_bw() +
   theme(legend.position = c(0.6,0.1),
         legend.direction = 'horizontal',
-        legend.text = element_text(size=16),
+        legend.text = element_text(size=12),
         legend.key.width = unit(2,'cm'),
         legend.key.height = unit(0.5,'cm'),
         axis.title = element_text(size=18),
-        axis.text = element_text(size=12))
+        axis.text = element_text(size=12)) +
+  guides(color=guide_legend(nrow=3))
 
 ####
-ggplot(habit_forecast_error, aes(x=lead_time, y=skill, color=as.factor(habit))) + 
+ggplot(habit_forecast_error, aes(x=lead_time, y=skill, color=as.factor(habit_with_n))) + 
   geom_line(size=1.5) +
-  scale_color_manual(values=c( "#F0E442", "#0072B2", "#D55E00", "#CC79A7")) + 
+  scale_color_manual(values=c( '#009E73','#0072B2')) + 
   geom_segment(data=indicator_lines, aes(x=x, xend=xend, y=y, yend=yend), size=0.8, arrow = arrow(length=unit(0.25,'cm')),
                inherit.aes = F) +
   geom_text(data=indicator_text, aes(x=x,y=y, label=t),size=4.5, inherit.aes = F) +
-  ylim(-0.5,0.4) +
+  ylim(ylim_low, ylim_high) +
   scale_x_continuous(labels = function(x){x*-1}) + 
   geom_hline(yintercept = 0, linetype='dashed', size=1) + 
-  ggtitle('By Habit') + 
+  #ggtitle('By Habit') + 
   labs(y='Skill',x='Lead time in days', color='') + 
   theme_bw() +
   theme(legend.position = c(0.6,0.1),
         legend.direction = 'horizontal',
-        legend.text = element_text(size=18),
+        legend.text = element_text(size=14),
         legend.key.width = unit(2,'cm'),
         legend.key.height = unit(0.5,'cm'),
         axis.title = element_text(size=18),
-        axis.text = element_text(size=12))
+        axis.text = element_text(size=12)) +
+  guides(color=guide_legend(nrow=2))
 
 sample_sizes = forecast_error %>%
   select(species, Phenophase_ID, lead_time, n) %>%
   distinct()
 
-###
-# All the species with phenophase color coded
+###########################################
+#### All the species with phenophase color coded
+###########################################
 ggplot(species_forecast_error, aes(x=lead_time, y=skill, color=Phenophase_ID)) + 
   geom_line(size=1.5) +
   scale_color_manual(values=c("#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")) + 
@@ -257,7 +268,7 @@ ggplot(species_forecast_error, aes(x=lead_time, y=skill, color=Phenophase_ID)) +
   #geom_segment(data=indicator_lines, aes(x=x, xend=xend, y=y, yend=yend), size=0.8, arrow = arrow(length=unit(0.25,'cm')),
   #             inherit.aes = F) +
   #geom_text(data=indicator_text, aes(x=x,y=y, label=t),size=4.5, inherit.aes = F) +
-  ylim(-0.5,0.8) +
+  ylim(-0.25,0.25) +
   scale_x_continuous(labels = function(x){x*-1}) + 
   geom_hline(yintercept = 0, linetype='dashed', size=1) + 
   labs(y='Skill',x='Lead time in days', color='') + 
@@ -271,8 +282,8 @@ ggplot(species_forecast_error, aes(x=lead_time, y=skill, color=Phenophase_ID)) +
         axis.title = element_text(size=18),
         axis.text = element_text(size=12))
 
-sample_sizes = forecast_error %>%
-  select(species, Phenophase_ID, lead_time, n) %>%
+sample_sizes = species_forecast_error %>%
+  select(species, Phenophase_ID, n) %>%
   distinct()
 
 ########################################################
@@ -307,60 +318,45 @@ sample_sizes = forecast_error %>%
 #   geom_line() +
 #   facet_wrap(species~Phenophase_ID, scales='free_y')
 #   
-#########################################################
-# Overall lead time error of all species combined
-total_sample_sizes = forecast_error %>%
-  select(lead_time, n) %>% 
-  distinct() %>%
-  filter(lead_time%%4==0)
 
-forecast_error = forecast_error %>%
-  gather(error_type, error, naive_error, forecast_error)
+# #########################################################
+# # lead time error of all speices
+# species_forecast_error = forecast_data %>%
+#   group_by(species, Phenophase_ID, phenology_model, lead_time) %>%
+#   summarise(naive_error = sqrt(mean((naive_prediction - doy_observed)**2)),
+#             forecast_error = sqrt(mean((doy_prediction - doy_observed)**2)),
+#             skill = 1 - (forecast_error/naive_error),
+#             n=n()) %>%
+#   ungroup() 
+# 
+# for(s in unique(species_forecast_error$species)){
+#   print(s)
+#   #f(s == 'hamamelis virginiana') next
+#   plot_data = species_forecast_error %>%
+#     filter(species==s)
+#   plot_sample_sizes = plot_data %>%
+#     select(species, Phenophase_ID, lead_time, n) %>%
+#     distinct() %>%
+#     filter(lead_time%%8==0)
+# 
+#   #if(sample_size_371 < 10 & sample_size_501 < 10) next
+# 
+#   plot_data = plot_data %>%
+#     gather(error_type, error, naive_error, forecast_error)
+# 
+# p=ggplot(plot_data, aes(x=lead_time, y=error, group=interaction(phenology_model, error_type), color=as.factor(phenology_model), linetype=as.factor(error_type))) +
+#   #geom_point(size=3, alpha=0.4) +
+#   geom_line(size=1) +
+#   geom_text(data=plot_sample_sizes, aes(x=lead_time, y=-1, label=n),position = position_jitter(height=0.5,width=0), size=4.8, inherit.aes = F) + 
+#   #geom_smooth(method='loess') +
+#   #geom_hline(aes(yintercept = mean(naive_error))) +
+#   #ylim(-5,1) +
+#   facet_wrap(Phenophase_ID~species) +
+#   #annotate('text', x=-10, y=-4.2, label=annotation_text)+
+#   labs(x='lead time in days') +
+#   theme(strip.text = element_text(size=12),
+#         axis.text = element_text(size=14))
+# #print(p)
+# ggsave(file=paste0('lead_time_plots/',stringr::str_replace(s, ' ','_'),'.png'), plot=p, height = 20, width=60, units='cm')
+# }
 
-ggplot(forecast_error, aes(x=lead_time, y=error, group=interaction(phenology_model, error_type), color=as.factor(phenology_model), linetype=as.factor(error_type))) +
-  geom_line() +
-  ylim(-2,25) +
-  geom_text(data=total_sample_sizes, aes(x=lead_time, y=-1, label=n),position = position_jitter(height=2,width=0), size=2.8, inherit.aes = F)
-
-#########################################################
-# lead time error of all speices
-species_forecast_error = forecast_data %>%
-  group_by(species, Phenophase_ID, phenology_model, lead_time) %>%
-  summarise(naive_error = sqrt(mean((naive_prediction - doy_observed)**2)),
-            forecast_error = sqrt(mean((doy_prediction - doy_observed)**2)),
-            skill = 1 - (forecast_error/naive_error),
-            n=n()) %>%
-  ungroup() 
-
-for(s in unique(species_forecast_error$species)){
-  print(s)
-  #f(s == 'hamamelis virginiana') next
-  plot_data = species_forecast_error %>%
-    filter(species==s)
-  plot_sample_sizes = plot_data %>%
-    select(species, Phenophase_ID, lead_time, n) %>%
-    distinct() %>%
-    filter(lead_time%%8==0)
-
-  #if(sample_size_371 < 10 & sample_size_501 < 10) next
-
-  plot_data = plot_data %>%
-    gather(error_type, error, naive_error, forecast_error)
-
-p=ggplot(plot_data, aes(x=lead_time, y=error, group=interaction(phenology_model, error_type), color=as.factor(phenology_model), linetype=as.factor(error_type))) +
-  #geom_point(size=3, alpha=0.4) +
-  geom_line(size=1) +
-  geom_text(data=plot_sample_sizes, aes(x=lead_time, y=-1, label=n),position = position_jitter(height=0.5,width=0), size=4.8, inherit.aes = F) + 
-  #geom_smooth(method='loess') +
-  #geom_hline(aes(yintercept = mean(naive_error))) +
-  #ylim(-5,1) +
-  facet_wrap(Phenophase_ID~species) +
-  #annotate('text', x=-10, y=-4.2, label=annotation_text)+
-  labs(x='lead time in days') +
-  theme(strip.text = element_text(size=12),
-        axis.text = element_text(size=14))
-#print(p)
-ggsave(file=paste0('lead_time_plots/',stringr::str_replace(s, ' ','_'),'.png'), plot=p, height = 20, width=60, units='cm')
-}
-
-spp_of_interest = c('acer saccharum','quercus rubra','quercus velutina')
