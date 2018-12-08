252d053ba5e72c110df6dec4de3b651461a11be5
Shawn Taylor <shawntaylor@weecology.org>
2018-12-08 12:52:22 -0500

    some file cleanup

--- analysis/process_current_season_observations.R
+++ analysis/process_current_season_observations.R
@@ -14,6 +14,9 @@ all_observations = current_season_observations %>%
   mutate(species= tolower(paste(Genus,Species,sep=' ')), 
          year   = lubridate::year(Observation_Date),
          doy    = lubridate::yday(Observation_Date)) %>%
+  # Drop any observations where the phenophase was observed
+  # but an intensity wasn't entered. Potentially a lot of false positives.
+  #filter(!(status==1 & intensity=='-9999')) %>%
   process_phenology_observations(prior_obs_cutoff = 30)
 
 # add in lat/lon
@@ -23,9 +26,10 @@ site_info = current_season_sites %>%
 all_observations = all_observations %>%
   left_join(site_info, by='site_id')
 
-# Drop group sites
+# Drop these group sites where is a lot of disagreement
+# among observers
 all_observations = all_observations %>%
-  filter(Site_Type!='Group')
+  filter(!site_id %in% c(21468, 21470, 21471))
 
 if(sum(is.na(all_observations$latitude))>0){
   warning('some missing coordinates')
@@ -37,4 +41,4 @@ all_observations = all_observations %>%
   mutate(observation_id = 1:n()) %>%
   ungroup()
 
-write_csv(all_observations, config$phenology_current_season_observation_file)
+write_csv(all_observations, 'analysis/current_season_with_custom_summaries.csv')
