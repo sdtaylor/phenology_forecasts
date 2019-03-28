9322a31d2b1724c86abaa6db05641ae056eb54bd
Shawn Taylor <shawntaylor@weecology.org>
2019-03-28 12:51:24 -0400

    switch to individual phenometrics

--- evaluation/data_prep/process_current_season_observations.R
+++ evaluation/data_prep/process_current_season_observations.R
@@ -4,41 +4,34 @@ source('tools/tools.R')
 
 config = load_config()
 
-current_season_observations = read_csv('/home/shawn/data/phenology/npn_2018/status_intensity_observation_data.csv')
-current_season_sites = read_csv('/home/shawn/data/phenology/npn_2018/ancillary_site_data.csv')
+current_season_observations = read_csv('/home/shawn/data/phenology_forecasting/evaluation/phenology_data_2018/individual_phenometrics_data.csv')
+current_season_sites = read_csv('/home/shawn/data/phenology_forecasting/evaluation/phenology_data_2018/ancillary_site_data.csv')
 
-# Process to only yes observations preceeded by a no within 30 days
-all_observations = current_season_observations %>%
-  select(site_id = Site_ID, individual_id = Individual_ID, Phenophase_ID, Observation_Date, status = Phenophase_Status,
-         intensity_id = Intensity_Category_ID, intensity = Intensity_Value, Genus, Species) %>%
-  mutate(species= tolower(paste(Genus,Species,sep=' ')), 
-         year   = lubridate::year(Observation_Date),
-         doy    = lubridate::yday(Observation_Date)) %>%
-  # Drop any observations where the phenophase was observed
-  # but an intensity wasn't entered. Potentially a lot of false positives.
-  #filter(!(status==1 & intensity=='-9999')) %>%
-  process_phenology_observations(prior_obs_cutoff = 30)
+# Drop any conflicts where multiple observers disagree, or single observers
+# mark multiple onsets
+current_season_observations = current_season_observations %>%
+  filter(!Observed_Status_Conflict_Flag %in% c("MultiObserver-StatusConflict","OneObserver-StatusConflict")) %>%
+  filter(Multiple_FirstY==0)
 
-# add in lat/lon
-site_info = current_season_sites %>%
-  select(site_id=Site_ID, latitude=Latitude, longitude=Longitude, Site_Type)
+# Keep only observations that had a prior 'no' within 30 days
+current_season_observations = current_season_observations %>%
+  filter(NumDays_Since_Prior_No >0, NumDays_Since_Prior_No<=30)
 
-all_observations = all_observations %>%
-  left_join(site_info, by='site_id')
+# Correct observed date as midpoint between 1st 'no' and  most prior 'no'
+current_season_observations$First_Yes_DOY = ceiling(with(current_season_observations, First_Yes_DOY - (NumDays_Since_Prior_No/2)))
 
-# Drop these group sites where is a lot of disagreement
-# among observers
-all_observations = all_observations %>%
-  filter(!site_id %in% c(21468, 21470, 21471))
-
-if(sum(is.na(all_observations$latitude))>0){
-  warning('some missing coordinates')
-}
+# Rename to variables I like
+current_season_observations = current_season_observations %>%
+  select(site_id = Site_ID, individual_id = Individual_ID, Phenophase_ID, Genus, Species, 
+         latitude=Latitude, longitude=Longitude,
+         year=First_Yes_Year, doy=First_Yes_DOY) %>%
+  mutate(species= tolower(paste(Genus,Species,sep=' '))) 
 
 # Add in a unique observation ID for every species and phenophase
-all_observations = all_observations %>%
+current_season_observations = current_season_observations %>%
   group_by(species, Phenophase_ID) %>%
   mutate(observation_id = 1:n()) %>%
   ungroup()
 
-write_csv(all_observations, 'analysis/current_season_with_custom_summaries.csv')
+write_csv(current_season_observations, config$phenology_current_season_observation_file)
+
