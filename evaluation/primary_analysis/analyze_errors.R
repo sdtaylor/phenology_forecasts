10f8bfc703845dfac5d6493224cb7f6f1e17fcda
Shawn <shawntaylor@weecology.org>
2019-07-03 14:01:27 -0400

    start finalizing figures

--- evaluation/primary_analysis/analyze_errors.R
+++ evaluation/primary_analysis/analyze_errors.R
@@ -1,83 +1,237 @@
 library(tidyverse)
+library(viridis)
 source('tools/tools.R')
 
 config = load_config()
 ########################################
+# This script produces the following
+# Figure X: rmse/mae values over lead time for 2018 and 2019
+# Figure X: the gbm modelled errors for latitude x year
+# Figure X: the species specific errors (mae on x axis, species/phenophase list on y)
+
+#####################################
+#####################################
+# Setup the hindcast data for evaluation
+########################################
 # Hindcast specific functions located here
 source('evaluation/data_prep/load_hindcast_data.R')
 
-hindcasts_big_format = load_hindcast_data(hindcasts_file = paste0(config$data_folder,'evaluation/hindcast_data_2018.csv'),
-                                          observations_file = paste0(config$data_folder,'evaluation/phenology_2018_observations.csv'),
-                               year=2018)
+# hindcasts_big_format = load_hindcast_data(hindcasts_file = paste0(config$data_folder,'evaluation/hindcast_data_2018.csv'),
+#                                           observations_file = paste0(config$data_folder,'evaluation/phenology_2018_observations.csv'),
+#                                year=2018)
+# 
+# hindcasts = hindcasts_big_format %>%
+#   calculate_point_estimates() %>%
+#   calculate_lead_time(hindcast_prediction_levels = c('species','Phenophase_ID','site_id','year','observation_id'))
 
-hindcasts = hindcasts_big_format %>%
-  calculate_point_estimates() %>%
+hindcasts_18 = load_hindcast_data(hindcasts_file = paste0(config$data_folder,'evaluation/hindcast_data_2018.csv'),
+                                          observations_file = paste0(config$data_folder,'evaluation/phenology_2018_observations.csv'),
+                                          year=2018) %>%
+  calculate_point_estimates(hindcast_prediction_levels = c('species','Phenophase_ID','site_id','latitude','longitude','year','observation_id','issue_date')) %>%
   calculate_lead_time(hindcast_prediction_levels = c('species','Phenophase_ID','site_id','year','observation_id'))
+
+hindcasts_19 = load_hindcast_data(hindcasts_file = paste0(config$data_folder,'evaluation/hindcast_data_2019.csv'),
+                                  observations_file = paste0(config$data_folder,'evaluation/phenology_2019_observations.csv'),
+                                  year=2019) %>%
+  calculate_point_estimates(hindcast_prediction_levels = c('species','Phenophase_ID','site_id','latitude','longitude','year','observation_id','issue_date')) %>%
+  calculate_lead_time(hindcast_prediction_levels = c('species','Phenophase_ID','site_id','year','observation_id'))
+
+hindcasts = hindcasts_18 %>%
+  bind_rows(hindcasts_19)
+
 
 ####################################
 # Only keep observations with at least 90 days of hindcasts and discard any which are greater
 # than 90 days. This normalizes all predictions to the same lead time, and makes it so
 # all lead times have the same sample size. 
 obs_to_keep = hindcasts %>%
-  group_by(species, Phenophase_ID, observation_id) %>%
-  summarize(has_adequate_lead_time = min(lead_time) <= -90) %>%
+  group_by(species, Phenophase_ID, observation_id, year) %>%
+  summarize(has_adequate_lead_time = min(lead_time) <= -120) %>%
   ungroup() 
 hindcasts = hindcasts %>%
-  left_join(obs_to_keep, by=c('species','Phenophase_ID','observation_id')) %>%
+  left_join(obs_to_keep, by=c('species','Phenophase_ID','observation_id','year')) %>%
   filter(has_adequate_lead_time) %>%
   select(-has_adequate_lead_time) %>%
-  filter(lead_time >= -90, lead_time <=0)
+  filter(lead_time >= -120, lead_time <=0)
 
+# # also only keep species/phenophases with at least 10 validation observations in a year
+# obs_to_keep = hindcasts %>%
+#   group_by(species, Phenophase_ID, year) %>%
+#   summarize(has_10_obs = n_distinct(observation_id) >= 10) %>%
+#   ungroup() 
+# hindcasts = hindcasts %>%
+#   left_join(obs_to_keep, by=c('species','Phenophase_ID','year')) %>%
+#   filter(has_10_obs) %>%
+#   select(-has_10_obs)
 
+#####################################
 #####################################
 naive_data = load_naive_forecasts(naive_forecast_file = paste0(config$data_folder,'evaluation/naive_model_data_2018.csv'),
                                   year=2018) %>%
+  bind_rows(load_naive_forecasts(naive_forecast_file = paste0(config$data_folder,'evaluation/naive_model_data_2019.csv'),
+                                 year=2019)) %>%
   select(-doy_sd_naive)
 
 hindcasts = hindcasts %>%
-  left_join(naive_data, by=c('species','Phenophase_ID','site_id')) %>%
+  left_join(naive_data, by=c('species','Phenophase_ID','site_id','year')) %>%
   rename(primary_model = doy_prediction,
          naive_model = doy_prediction_naive) %>%
   gather(model_type, doy_prediction, primary_model, naive_model)
 
-
+#####################################
 ####################################
 
 hindcasts = hindcasts %>%
   mutate(error = doy_prediction - doy_observed)
 
+#####################################
 ####################################
-lead_time_errors = hindcasts %>%
-  group_by(lead_time, model_type) %>%
+# Figure 2: total RMSE vs lead time
+####################################
+#####################################
+yearly_lead_time_errors = hindcasts %>%
+  group_by(lead_time, model_type, year) %>%
   summarise(rmse = sqrt(mean(error**2)),
             mae = mean(abs(error)),
-            n=n()) %>%
+            n=n(),
+            n_species_phenophase = n_distinct(interaction(species, Phenophase_ID))) %>%
   ungroup()
-ggplot(lead_time_errors, aes(x=lead_time, y=rmse, color=model_type)) + 
-  geom_line()
 
+yearly_lead_time_errors$model_type = factor(yearly_lead_time_errors$model_type, levels = c('naive_model','primary_model'), labels = c('Naive','Primary'))
+yearly_lead_time_errors$year = factor(yearly_lead_time_errors$year, levels = c(2018,2019), labels = c('2018 Observations (n=837)','2019 Observations (n=1586)'))
+ggplot(yearly_lead_time_errors, aes(x=lead_time, y=mae, color=model_type)) + 
+  geom_line(size=3) +
+  scale_color_manual(values=c("black", "grey60")) + 
+  facet_wrap(~year, ncol=2) +
+  scale_x_continuous(breaks=seq(-120,0,20), labels = function(x){x*-1}) + 
+  theme_bw() + 
+  theme(text = element_text(size=24),
+        strip.text = element_text(size=28),
+        legend.text = element_text(size=30),
+        legend.key.width = unit(20,'mm'),
+        legend.title = element_blank(),
+        legend.background = element_rect(color='black'),
+        legend.position = c(0.3, 0.8)) +
+  labs(x = 'Lead Time in Days', y='Mean Absolute Error (MAE)', color='')
+
+# density of errors by year and naive/primary model
+ggplot(filter(hindcasts, lead_time==0), aes(x=error, color=model_type)) + 
+  geom_density(fill=NA) + 
+  geom_vline(xintercept = 0) + 
+  facet_wrap(~year, ncol=2) +
+  labs(x = 'Error (prediction - observed)')
+# In 2018 the naive model underpredicted, thus it was a late year for most phenology.
+# in 2019 the naive model is centered right around 0, thus it was a pretty normal year
+
+####################################
+# Figure XX: Ethan plot. mae split out by species/phenophase
+####################################
+abbreviate_species_names = function(s){                                                                                  
+  genus_abbr = paste0(toupper(substr(s, 1,1)), '. ')
+  specific_epithet = stringr::word(s, 2,2)
+  return(paste0(genus_abbr, specific_epithet))
+}
+
+species_errors = hindcasts %>%
+  group_by(species, Phenophase_ID, lead_time, year) %>%
+  summarise(me = mean(error),
+            sd = sd(error),
+            n=n()) %>%
+  ungroup() %>%
+  filter(lead_time %in% c(-90,0)) %>%
+  filter(n>=10)
+
+species_errors$Phenophase_ID = factor(species_errors$Phenophase_ID, levels=c(371,501),labels=c('Leaves','Flowers'))
+species_errors$species_label = as.factor(with(species_errors, paste0(abbreviate_species_names(species),' - ', Phenophase_ID)))
+# Rank the species by their error
+species_errors$species_label = forcats::fct_reorder(species_errors$species_label, -species_errors$me, .fun = min)
+
+# Setup a nudge based on lead time
+y_nudge_base = 0.15
+species_errors$y_nudge = with(species_errors, ifelse(lead_time==-90, y_nudge_base, y_nudge_base*-1))
+
+ggplot(species_errors ,aes(x=me, y=species_label, color=as.factor(lead_time))) +
+  geom_vline(xintercept = 0, size=1, alpha=0.8) +
+  geom_point(size=2, position = position_nudge(x=0, y=species_errors$y_nudge)) +
+  geom_errorbarh(aes(xmin = me-sd, xmax = me+sd),  position = position_nudge(x=0, y=species_errors$y_nudge),
+                 size=0.8, height = 0.3) + 
+  # # again for the other lead time so that these points can be nudged up slightly
+  # geom_point(data = filter(species_errors, lead_time==-90), size=2, position = position_nudge(x=0, y=y_nudge)) +
+  # geom_errorbarh(data = filter(species_errors, lead_time==-90), aes(xmin = me-sd, xmax = me+sd),  position = position_nudge(x=0, y=y_nudge),
+  #                size=0.8, height = 0.3) + 
+  scale_color_manual(values = c("#0072B2", "#D55E00")) + 
+  #xlim(-20,20) + 
+  facet_wrap(~year,ncol=2) + 
+  theme_bw() +
+  theme(axis.text.y = element_text(size=10),
+        axis.title = element_text(size=20),
+        legend.text = element_text(size=14),
+        legend.title = element_text(size=14),
+        axis.text.x = element_text(size=14)) + 
+  labs(y='Species - Phenophase (evaluation sample size)',x='Mean Error in Days ± 1 S.D.', color='Lead Time\nin Days')
+
+#####################################
 #####################################
 # The error model
 #####################################
+#####################################
+# acer rubrum errors only
+
+ggplot(filter(hindcasts, species=='acer rubrum'), aes(x=error,y=latitude,color=as.factor(lead_time))) + 
+  geom_point()
+
+ggplot(filter(hindcasts, species=='acer rubrum'), aes(x=error,color=as.factor(Phenophase_ID))) + 
+  geom_density() +
+  geom_vline(xintercept = 0)
+
+#####################################
 library(gbm)
-library(randomForest)
 library(pdp)
 
 hindcasts$Phenophase_ID = as.factor(hindcasts$Phenophase_ID)
 hindcasts$species = as.factor(hindcasts$species)
+hindcasts$year = as.factor(hindcasts$year)
 
-error_model = randomForest(error ~  Phenophase_ID + species + latitude + longitude + lead_time, 
-                  n.trees = 500,
-                  data=hindcasts)
+error_model = gbm(error ~  species + Phenophase_ID  + latitude + longitude + lead_time + year, 
+                  n.trees = 5000, interaction.depth = 3, shrinkage = 0.01, n.cores = 4,
+                  data=filter(hindcasts, model_type=='primary_model'))
+gbm::gbm.perf(error_model, plot.it = T)
 
-gbm::relative.influence(error_model)
+#######################################
+# Figure 5: Modelled errors with respect to latitude
+######################################
+partial(error_model, pred.var = c('latitude','year'), n.trees=5000, plot=F) %>%
+  ggplot(aes(y=latitude, x=yhat, color=as.factor(year))) + 
+  geom_path(size=3) +
+  scale_color_manual(values=c("black", "grey60")) + 
+  geom_vline(xintercept = 0) + 
+  labs(x='Modelled Error (prediction - observed)', y='Latitude',color='',
+       title='') +
+  theme_bw() + 
+  theme(text=element_text(size=20),
+        legend.position = c(0.25, 0.8),
+        legend.title = element_blank(),
+        legend.text = element_text(size=25),
+        legend.key.width = unit(20,'mm'),
+        legend.background = element_rect(color='black'))
 
+#######################################
+# Figure XX: Modelled errors with respect to lead time
+######################################
+partial(error_model, pred.var = c('lead_time','year'), n.trees=5000, plot=F) %>%
+  ggplot(aes(y=yhat, x=lead_time, color=as.factor(year))) + 
+  geom_path(size=3) +
+  scale_color_manual(values=c("black", "grey60")) + 
+  geom_vline(xintercept = 0) + 
+  labs(y='Modelled Absolute Error (prediction - observed)', x='Lead Time',color='',
+       title='') +
+  theme_bw() + 
+  theme(text=element_text(size=20),
+        legend.position = c(0.25, 0.15),
+        legend.title = element_blank(),
+        legend.text = element_text(size=25),
+        legend.key.width = unit(20,'mm'),
+        legend.background = element_rect(color='black'))
 
-species_pdp_data = partial(error_model, pred.var = c('longitude','latitude','lead_time'), n.trees=500)
-
-ggplot(filter(species_pdp_data, lead_time==0), aes(x=longitude, y=latitude, fill=yhat)) + 
-  geom_raster() +
-  scale_fill_viridis(discrete = F) +
-  theme(legend.position = 'right')
 
 
---
+++ evaluation/primary_analysis/analyze_uncertainty.R
@@ -0,0 +1,167 @@
+library(tidyverse)
+library(viridis)
+source('tools/tools.R')
+
+config = load_config()
+
+########################################
+# This script produces the following
+# Figure X: rmse/mae values over lead time for 2018 and 2019
+# Figure X: the gbm modelled errors for latitude x year
+# Figure X: the species specific errors (mae on x axis, species/phenophase list on y)
+
+#####################################
+#####################################
+# Setup the hindcast data for evaluation
+########################################
+# Hindcast specific functions located here
+source('evaluation/data_prep/load_hindcast_data.R')
+
+hindcasts_big_format = load_hindcast_data(hindcasts_file = paste0(config$data_folder,'evaluation/hindcast_data_2018.csv'),
+                                          observations_file = paste0(config$data_folder,'evaluation/phenology_2018_observations.csv'),
+                               year=2018) %>%
+  calculate_lead_time(hindcast_prediction_levels = c('species','Phenophase_ID','site_id','phenology_model','year','climate_member','observation_id'))
+
+# hindcasts_18 = load_hindcast_data(hindcasts_file = paste0(config$data_folder,'evaluation/hindcast_data_2018.csv'),
+#                                   observations_file = paste0(config$data_folder,'evaluation/phenology_2018_observations.csv'),
+#                                   year=2018) %>%
+#   calculate_lead_time(hindcast_prediction_levels =c('species','Phenophase_ID','site_id','phenology_model','year','climate_member','observation_id'))
+# 
+# hindcasts_19 = load_hindcast_data(hindcasts_file = paste0(config$data_folder,'evaluation/hindcast_data_2019.csv'),
+#                                   observations_file = paste0(config$data_folder,'evaluation/phenology_2019_observations.csv'),
+#                                   year=2019) %>%
+#   calculate_lead_time(hindcast_prediction_levels =c('species','Phenophase_ID','site_id','phenology_model','year','climate_member','observation_id'))
+# 
+# hindcasts_big_format = hindcasts_18 %>%
+#   bind_rows(hindcasts_19)
+
+####################################
+# Only keep observations with at least 90 days of hindcasts and discard any which are greater
+# than 90 days. This normalizes all predictions to the same lead time, and makes it so
+# all lead times have the same sample size. 
+obs_to_keep = hindcasts_big_format %>%
+  group_by(species, Phenophase_ID, observation_id, year) %>%
+  summarize(has_adequate_lead_time = min(lead_time) <= -120) %>%
+  ungroup() 
+hindcasts_big_format = hindcasts_big_format %>%
+  left_join(obs_to_keep, by=c('species','Phenophase_ID','observation_id','year')) %>%
+  filter(has_adequate_lead_time) %>%
+  select(-has_adequate_lead_time) %>%
+  filter(lead_time >= -120, lead_time <=0)
+
+##################################################################
+# calculate uncertainty for each component
+#####################################
+
+total_uncertainty = hindcasts_big_format %>%
+  group_by(species, Phenophase_ID, site_id, year, lead_time, observation_id) %>%
+  summarise(total_sd = sd(doy_prediction),
+            doy_observed = unique(doy_observed),
+            doy_prediction = mean(doy_prediction)) %>%
+  ungroup()
+
+parameter_uncertainty = hindcasts_big_format %>%
+  group_by(species, Phenophase_ID, site_id,observation_id, year, lead_time,    phenology_model, climate_member) %>%
+  summarise(parameter_sd = sd(doy_prediction)) %>%
+  ungroup() %>%
+  group_by(species, Phenophase_ID, site_id,observation_id, year, lead_time) %>%
+  summarise(parameter_sd = mean(parameter_sd)) %>%
+  ungroup()
+
+model_uncertainty = hindcasts_big_format %>%
+  group_by(species, Phenophase_ID, site_id, observation_id, year, lead_time,    phenology_model, climate_member) %>%
+  summarise(doy_prediction = mean(doy_prediction)) %>%
+  ungroup() %>%
+  group_by(species, Phenophase_ID, site_id, observation_id, year, lead_time,    climate_member) %>%
+  summarise(model_sd = sd(doy_prediction)) %>%
+  ungroup() %>%
+  group_by(species, Phenophase_ID, site_id, observation_id, year, lead_time) %>%
+  summarise(model_sd = mean(model_sd)) %>%
+  ungroup()
+
+climate_uncertainty = hindcasts_big_format %>%
+  group_by(species, Phenophase_ID, site_id, observation_id,year, lead_time,    climate_member) %>%
+  summarise(doy_prediction = mean(doy_prediction)) %>%
+  ungroup() %>%
+  group_by(species, Phenophase_ID, site_id, observation_id,year, lead_time) %>%
+  summarise(climate_sd = sd(doy_prediction)) %>%
+  ungroup()
+
+all_uncertainty = total_uncertainty %>%
+  left_join(parameter_uncertainty, by=c('species','Phenophase_ID','site_id','observation_id','year','lead_time')) %>%
+  left_join(model_uncertainty, by=c('species','Phenophase_ID','site_id','observation_id','year','lead_time')) %>%
+  left_join(climate_uncertainty, by=c('species','Phenophase_ID','site_id','observation_id','year','lead_time')) %>%
+  gather(uncertainty_source,SD, total_sd, parameter_sd, model_sd, climate_sd)
+
+aggregate_uncertainty = all_uncertainty %>%
+  group_by(uncertainty_source, lead_time) %>%
+  summarise(SD = mean(SD, na.rm = T),
+            sum_na = sum(is.na(SD)),
+            n=n())
+
+aggregate_uncertainty$uncertainty_source = factor(aggregate_uncertainty$uncertainty_source,
+                                                  levels=c('total_sd','model_sd','parameter_sd','climate_sd'),
+                                                  labels=c('Total','Model','Parameter','Climate'),
+                                                  ordered = T)
+####################################################################
+# Figure XX: total/parameter/model/climate uncertainty over lead time (ie. the ribbon plot)
+#####################################
+
+ggplot(aggregate_uncertainty, aes(x=lead_time, y=0, fill=uncertainty_source)) + 
+  geom_ribbon(aes(ymin=-SD*1.96, ymax=SD*1.96), alpha=0.8, size=1.2) +
+  scale_fill_manual(values=c('#4b3f72','#ffc857','#119da4','grey10')) +
+  theme_bw() + 
+  theme(text=element_text(size=20),
+        legend.position = 'bottom') +
+  labs(x='Lead Time', y = '95% CI', fill='Uncertainty Source')
+  
+######################################################################
+# Figure XX: forecast coverage for each of the sources of uncertainty
+###################################################
+forecast_coverage = all_uncertainty %>%
+  mutate(p = pnorm(doy_observed, doy_prediction, SD)) %>%
+  group_by(lead_time, uncertainty_source) %>%
+  summarise(coverage = mean(p > 0.025 & p < 0.975, na.rm=T)) %>%
+  ungroup() %>%
+  mutate(year=2018) %>%
+  mutate(uncertainty_source = factor(uncertainty_source,
+                                                        levels=c('total_sd','model_sd','parameter_sd','climate_sd'),
+                                                        labels=c('Total','Model','Parameter','Climate'),
+                                                        ordered = T))
+
+forecast_coverage$uncertainty_source = factor(forecast_coverage$uncertainty_source,
+                                                  levels=c('total_sd','model_sd','parameter_sd','climate_sd'),
+                                                  labels=c('Total','Model','Parameter','Climate'),
+                                                  ordered = T)
+
+ggplot(forecast_coverage,aes(x=lead_time, y=coverage, color=uncertainty_source)) + 
+  geom_line(size=2) +
+  scale_color_manual(values=c('#4b3f72','#ffc857','#119da4','grey10')) +
+  scale_y_continuous(breaks=seq(0,1,0.2), limits = c(0,1)) + 
+  scale_x_continuous(breaks = seq(-120,0,20), labels = function(d){d*-1}) + 
+  geom_hline(yintercept = 0.95, size=1, linetype='dashed') + 
+  theme_bw() + 
+  theme(text=element_text(size=20),
+        legend.position = 'bottom') +
+  labs(x='Lead Time', y = 'Coverage',color='Uncertainty Source')
+
+######################################################################
+# Ethan plot, coverage of just total vs climate only, split out by year
+##########################################################################
+# must save 2019 first
+forecast_coverage_2019 = forecast_coverage %>%
+  mutate(year=2019)
+
+
+forecast_coverage_2018 %>%
+  bind_rows(forecast_coverage_2019) %>%
+  filter(uncertainty_source %in% c('Total','Climate')) %>% 
+  ggplot(aes(x=lead_time, y=coverage, color=as.factor(year))) + 
+  geom_line(size=4) +
+  scale_color_manual(values=c('#4b3f72','#ffc857','#119da4','grey10')) +
+  geom_hline(yintercept = 0.95, size=1, linetype='dashed') + 
+  ylim(0,1) + 
+  facet_wrap(~uncertainty_source, ncol=2) + 
+  theme_bw() + 
+  theme(text=element_text(size=20),
+        legend.position = 'bottom') 
