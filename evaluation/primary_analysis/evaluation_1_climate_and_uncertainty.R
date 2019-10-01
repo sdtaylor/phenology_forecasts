83f247e9cb118126a9cd6e0fbbcd07ddd1805793
Shawn <shawntaylor@weecology.org>
2019-10-01 08:54:44 -0400

    drop everything lead time related

--- evaluation/primary_analysis/evaluation_1_climate_and_uncertainty.R
+++ evaluation/primary_analysis/evaluation_1_climate_and_uncertainty.R
@@ -25,54 +25,19 @@ hindcasts_primary_method = load_hindcast_data(hindcasts_file = paste0(config$dat
   calculate_point_estimates(hindcast_prediction_levels = c('species','Phenophase_ID','site_id','latitude','longitude','year','observation_id','issue_date'),
                             add_sd=T) 
 
-hindcasts_primary_method_with_lead_times = hindcasts_primary_method %>%
-  dplyr::group_split(species, Phenophase_ID) %>%
-  purrr::map_dfr( .f = function(x){calculate_lead_time(hindcasts = x,hindcast_prediction_levels =  c('species','Phenophase_ID','site_id','year','observation_id'))})
-
 # And the one to compare with which uses only observations (PRISM) integrated with long term averages
 # the group_split %> map_dfr is important here to keep memory usage reasonable. Files are 4-9 GB
-hindcasts_lta_method = load_hindcast_data(hindcasts_file = paste0(config$data_folder,'evaluation/hindcast_lta_method_data_5_climates_2018.csv'),
+hindcasts_lta_method = load_hindcast_data(hindcasts_file = paste0(config$data_folder,'evaluation/hindcast_lta_method_data_2018.csv'),
                                       observations_file = paste0(config$data_folder,'evaluation/phenology_2018_observations.csv'),
                                       year=2018) %>%
   calculate_point_estimates(hindcast_prediction_levels = c('species','Phenophase_ID','site_id','latitude','longitude','year','observation_id','issue_date'),
                             add_sd=T) 
 
-hindcasts_lta_method_with_lead_times = hindcasts_lta_method %>%
-  dplyr::group_split(species, Phenophase_ID) %>%
-  purrr::map_dfr( .f = function(x){calculate_lead_time(hindcasts = x,hindcast_prediction_levels =  c('species','Phenophase_ID','site_id','year','observation_id'))})
-
-
 hindcasts_primary_method$method = 'primary'
-hindcasts_primary_method_with_lead_times$method = 'primary'
 hindcasts_lta_method$method = 'observed_temp_only'
-hindcasts_lta_method_with_lead_times$method = 'observed_temp_only'
 
 hindcasts_with_issue_dates = hindcasts_primary_method %>%
   bind_rows(hindcasts_lta_method)
-
-hindcasts_with_lead_times = hindcasts_primary_method_with_lead_times %>%
-  bind_rows(hindcasts_lta_method_with_lead_times)
-
-####################################
-# For forecasts with lead tiems only keep observations with at least 120 days of hindcasts and discard any issue dates
-# which are greater # than 120 days. This normalizes all predictions to the same lead time, and makes it so
-# all lead times have the same sample size. 
-obs_to_keep = hindcasts_with_lead_times %>%
-  group_by(species, Phenophase_ID, observation_id, year) %>%
-  summarize(has_adequate_lead_time = min(lead_time) <= -120) %>%
-  ungroup()
-hindcasts_with_lead_times = hindcasts_with_lead_times %>%
-  left_join(obs_to_keep, by=c('species','Phenophase_ID','observation_id','year')) %>%
-  filter(has_adequate_lead_time) %>%
-  select(-has_adequate_lead_time) %>%
-  filter(lead_time >= -120, lead_time <=0) %>%
-  group_by(species, Phenophase_ID, observation_id, method) %>%
-  filter(n() == 121) %>% # some obs (about 20 total) flowered/budbursted after May 30 (the last hindcast day) so don't have exactly 120 days of lead time
-  ungroup() %>%
-  I()
-
-# Potentially drop the same ones in the issue date methods so sample sizes are the same
-
 
 #####################################
 #####################################
@@ -95,21 +60,10 @@ hindcasts_with_issue_dates = hindcasts_with_issue_dates %>%
                names_to = c('.value','method'),
                names_pattern = '(doy_prediction_sd|doy_prediction)_(primary|observed_temp_only|lta)')
 
-hindcasts_with_lead_times = hindcasts_with_lead_times %>%
-  select(-issue_date, -issue_doy) %>%
-  pivot_wider(names_from = 'method', values_from = c('doy_prediction','doy_prediction_sd')) %>%
-  left_join(long_term_average_data, by=c('species','Phenophase_ID','site_id','year')) %>%
-  pivot_longer(cols = contains('doy_prediction'),
-               names_to = c('.value','method'),
-               names_pattern = '(doy_prediction_sd|doy_prediction)_(primary|observed_temp_only|lta)')
-
 #####################################
 ####################################
 
 hindcasts_with_issue_dates = hindcasts_with_issue_dates %>%
-  mutate(error = doy_prediction - doy_observed)
-
-hindcasts_with_lead_times = hindcasts_with_lead_times %>%
   mutate(error = doy_prediction - doy_observed)
 
 #####################################
@@ -131,11 +85,12 @@ plot_theme =  theme_bw() +
 
 line_colors = c('#E69F00',"black", "grey60")
 
-rmse_range = c(9,17.5)
+rmse_range = c(8,24)
 coverage_range = c(0.6, 0.98)
 
 # Issue date plots
 issue_date_errors = hindcasts_with_issue_dates %>%
+  #filter(species == 'acer rubrum') %>%
   mutate(p = pnorm(doy_observed, doy_prediction, doy_prediction_sd)) %>% # probability of observation given prediction, used for coverage
   group_by(issue_date, year, method) %>%
   summarise(rmse = sqrt(mean(error**2, na.rm = T)),
@@ -151,18 +106,17 @@ issue_date_errors$method = factor(issue_date_errors$method,
                                   levels = c('lta','observed_temp_only','primary'), 
                                   labels = c('Long Term Average','Observed Temp. Integration Only','Observed Temp. + Forecasts Integration'))
 
-issue_date_rmse_plot = ggplot(issue_date_errors, aes(x=issue_date, y=mae, color=method)) + 
+issue_date_rmse_plot = ggplot(issue_date_errors, aes(x=issue_date, y=rmse, color=method)) + 
   geom_line(size=3) +
   scale_color_manual(values = line_colors) + 
   scale_x_date(breaks = as.Date(c('2017-12-01','2018-01-01', '2018-02-01', '2018-03-01', '2018-04-01', '2018-05-01', '2018-06-01'))) + 
-  scale_y_continuous(limits = rmse_range) + 
+  #scale_y_continuous(limits = c(14,23)) + 
   labs(y='RMSE', title = 'A. RMSE with respect to issue date') +
   plot_theme +
   theme(axis.title.x = element_blank(),
         axis.text.x = element_blank(),
         axis.ticks.x = element_blank(),
         legend.position = 'none')
-
 
 issue_date_coverage_plot = ggplot(issue_date_errors, aes(x=issue_date, y=coverage, color=method)) + 
   geom_line(size=3) +
@@ -171,55 +125,14 @@ issue_date_coverage_plot = ggplot(issue_date_errors, aes(x=issue_date, y=coverag
   scale_color_manual(values = line_colors) + 
   scale_x_date(breaks = as.Date(c('2017-12-01','2018-01-01', '2018-02-01', '2018-03-01', '2018-04-01', '2018-05-01', '2018-06-01')),
                labels = function(x){format(x,'%b. %d')}) + 
-  scale_y_continuous(limits = coverage_range) + 
+  #scale_y_continuous(limits = coverage_range) + 
   labs(y = 'Coverage', x= 'Issue Date', title = 'B. Coverage with respect to issue date') +
-  plot_theme 
-
-# Lead time plots
-lead_time_errors = hindcasts_with_lead_times %>%
-  mutate(p = pnorm(doy_observed, doy_prediction, doy_prediction_sd)) %>% # probability of observation given prediction, used for coverage
-  group_by(lead_time, year, method) %>%
-  summarise(rmse = sqrt(mean(error**2, na.rm = T)),
-            mae = mean(abs(error), na.rm = T),
-            coverage = mean(p > 0.025 & p < 0.975, na.rm=T),
-            n=n(),
-            num_na = sum(is.na(error)),
-            n_species_phenophase = n_distinct(interaction(species, Phenophase_ID))) %>%
-  ungroup()
-
-
-lead_time_errors$method = factor(lead_time_errors$method, 
-                                  levels = c('lta','observed_temp_only','primary'), 
-                                  labels = c('Long Term Average','Observed Temp. Integration Only','Observed Temp. + Forecasts Integration'))
-
-lead_time_rmse_plot = ggplot(lead_time_errors, aes(x=lead_time, y=rmse, color=method)) + 
-  geom_line(size=3) +
-  scale_color_manual(values = line_colors) + 
-  scale_x_continuous(breaks=seq(-120,0,20), labels = function(x){x*-1}) + 
-  scale_y_continuous(limits = rmse_range) + 
-  labs(y='', title = 'C. RMSE with respect to lead time') +
-  plot_theme +
-  theme(axis.title.x = element_blank(),
-        axis.text.x = element_blank(),
-        axis.ticks.x = element_blank(),
-        legend.position = 'none')
-
-
-lead_time_coverage_plot = ggplot(lead_time_errors, aes(x=lead_time, y=coverage, color=method)) + 
-  geom_line(size=3) +
-  #geom_segment(y=0.95, yend=0.95, x=-120, xend=0, color='black', linetype='dashed', size=2) +
-  geom_hline(yintercept = 0.95, size=2, linetype='dotted') + 
-  scale_color_manual(values = line_colors) + 
-  scale_x_continuous(breaks=seq(-120,0,20), labels = function(x){x*-1}) + 
-  scale_y_continuous(limits = coverage_range) + 
-  labs(y = '', x= 'Lead Time', title = 'D. Coverage with respect to lead time') +
   plot_theme 
 
 ###############################################
 
 
-issue_date_rmse_plot + lead_time_rmse_plot +
-  issue_date_coverage_plot + lead_time_coverage_plot
+issue_date_rmse_plot + issue_date_mae_plot +  issue_date_coverage_plot + plot_layout(ncol=1)
 
 
 
